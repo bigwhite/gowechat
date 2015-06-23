e8068931981e478ba6bb7e8caa6aeaf37a22c9a1
Tony Bai <bigwhite.cn@gmail.com>
2015-06-23 16:29:19 +0800

    add example_test for qy

---
+++ qy/example_test.go
@@ -0,0 +1,53 @@
+package pb_test
+
+import (
+	"fmt"
+
+	"github.com/bigwhite/gowechat/qy"
+)
+
+const (
+	accessToken = "wx1234abcd"
+	layout      = `{
+	    "button": [
+			{
+				"name":"submenu1",
+				"subbutton": [
+				    {
+						"name": "item1",
+						"type":"click",
+						"key":"s1-item1"
+					},
+				    {
+						"name": "item2",
+						"type":"click",
+						"key":"s1-item2"
+					}
+				]
+			},
+			{
+				"name":"submenu2",
+				"subbutton": [
+				    {
+						"name": "item1",
+						"type":"click",
+						"key":"s2-item1"
+					},
+				    {
+						"name": "item2",
+						"type":"click",
+						"key":"s2-item2"
+					}
+				]
+			}
+		]
+} `
+	agentID = "5"
+)
+
+func ExampleCreateMenu() {
+	err := qy.CreateMenu([]byte(layout), accessToken, agentID)
+	fmt.Println(err)
+	// Output: https://qyapi.weixin.qq.com/cgi-bin/menu/create?access_token=wx1234abcd&agentid=5
+	// invalid access_token
+}
